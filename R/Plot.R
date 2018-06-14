81e9dbb2e1b98d142a2ff6e3ebbe6e40341da3dc
Steve Sansom <stephen.sansom@kennedy.ox.ac.uk>
2018-06-14 11:39:51 +0100

    refine sampleEnrichmentHeatmap

--- R/Plot.R
+++ R/Plot.R
@@ -1,3 +1,28 @@
+#' Utility function to clean up desriptions for plots
+#' 
+#' @param descriptions Vector of descriptions to clean
+#' @param remove Vector of string patterns to delete 
+#' @param maxl  Max length of the descriptions
+#' 
+#' @export
+formatDescriptions <- function(xx,
+                               remove=c(),
+                               maxl=45)
+{
+  for(rmstr in remove)
+  {
+  xx <- gsub(rmstr, "", xx)
+  }
+  xx <- gsub("_", " ", xx)
+  
+  # covert all upper case strings to all lower case. 
+  xx <- sapply(xx, function(x) if(x == toupper(x)) { tolower(x) } else { x }) 
+
+  xx[is.na(xx)] <- "n/a"
+  xx[nchar(xx)>maxl] <- paste0(strtrim(xx[nchar(xx)>maxl], maxl), "'")
+  xx
+}
+
 #' Draw a heatmap of enriched genesets in all samples
 #'
 #' @param results_table A \emph{gofisher} multi-sample results table.
@@ -5,6 +30,7 @@
 #' @param adjust_pvalues Logical value indicating whether to adjust p-values
 #' across all sample.
 #' @param pvalue_threshold P-value threshold.
+#' @param min_odds_ratio Only genesets with odds ratios equal to or greater will be considered.
 #' @param maxl Maximum length for gene set names (trimmed otherwise).
 #' @param show_common Logical value indicating whether to show gene sets
 #' enriched in all samples.
@@ -13,6 +39,7 @@
 #' (see \code{\link{p.adjust.methods}}).
 #' @param title Title of the heat map plot.
 #' @param sample_id_col The column of the results_table containing the sample id
+#' @param sample_ids A vector of sample identifiers to report. Can be used to subset the frame, or to add missing columns.
 #'
 #' @importFrom gplots heatmap.2
 #' @importFrom reshape2 dcast
@@ -32,6 +59,7 @@ sampleEnrichmentHeatmap <- function(
                                     results_table,
                                     max_rows=50,
                                     min_genes=2,
+                                    min_odds_ratio=1.5,
                                     p_col="p.val",
                                     adjust_pvalues=FALSE,
                                     padjust_method="BH",
@@ -39,19 +67,25 @@ sampleEnrichmentHeatmap <- function(
                                     maxl=45,
                                     show_common=TRUE,
                                     sample_id_col="cluster",
+                                    sample_ids=NULL,
                                     title="Enriched gene sets"
                                     )
 {
   
   results_table[[sample_id_col]] <- as.character(results_table[[sample_id_col]])
-  
-    sample_names <- unique(results_table[[sample_id_col]])
-    total_n_sample <- length(sample_names)
 
-    ## Filter the gene sets
-    results_table <- results_table[
-        results_table$n_fg >= min_genes & !is.na(results_table$n_fg),]
+  # handle the sample identifiers
+  if(!is.null(sample_ids))
+  {
+    sample_ids <- as.character(sample_ids)
+    results_table <- results_table[results_table[[sample_id_col]] %in% sample_ids,] 
+  } else {
+    sample_ids <- unique(results_table[[sample_id_col]])
+  }
+   
+  total_n_sample <- length(sample_ids)
 
+    ## (Adjust) and filter on p-values before other filters are applied
     if (adjust_pvalues) {
         ## compute FDR accross all samples
         message("filtering on adjusted p values")
@@ -67,6 +101,14 @@ sampleEnrichmentHeatmap <- function(
     if (!show_common) {
         results_table <- results_table[results_table$n_sample < total_n_sample, ]
     }
+    
+    ## Filter based on number of genes in the foreground
+    results_table <- results_table[
+      results_table$n_fg >= min_genes & !is.na(results_table$n_fg),]
+    
+    ## Filter based on odds ratio
+    results_table <- results_table[
+      results_table$odds.ratio >= min_odds_ratio & !is.na(results_table$odds.ratio),]
 
     ## Check for gene sets after filtering
     if (nrow(results_table) == 0) {
@@ -109,9 +151,9 @@ sampleEnrichmentHeatmap <- function(
 
     # process the term description
     xx <- temp$description
-    xx[is.na(xx)] <- "n/a"
-    xx[nchar(xx)>maxl] <- paste0(strtrim(xx[nchar(xx)>maxl], maxl), "'")
+    xx <- formatDescriptions(xx, c("REACTOME_", "BIOCARTA_"), maxl)
     temp$description <- xx
+    
     lu <- unique(temp[,c("geneset_id", "description")])
     lu$description <- make.unique(lu$description)
     rownames(lu) <- lu$geneset_id
@@ -122,11 +164,9 @@ sampleEnrichmentHeatmap <- function(
     dd$geneset_id <- NULL
 
     ## deal with missing samples
-    for (sample in sample_names) {
+    for (sample in sample_ids) {
         if (!sample %in% colnames(dd)) {
             message("adding column: ", sample)
-            cnames <- colnames(dd)
-            
             dd[[sample]] <- NA
         }
     }
@@ -181,10 +221,10 @@ sampleEnrichmentHeatmap <- function(
     # specify the color range as 
     # minimum = minimum non-NA value
     # maximum = equivalent to an odds ratio of 256.
-    rm <- c(min(mtrans[!is.na(mtrans)]), 8)
+    rm <- c(min(log2(min_odds_ratio + 1)), 8)
     breaks=seq(rm[1], rm[2], diff(rm) / nbreaks)
     
-    colors <- colorRampPalette(brewer.pal(50, "YlOrRd"))
+    colors <- colorRampPalette(brewer.pal(9, "YlOrRd"))
 
     if(adjust_pvalues)
     {
@@ -197,7 +237,7 @@ sampleEnrichmentHeatmap <- function(
     }
 
     op <- par() # backup current session settings
-    par(cex.main=0.7)
+    par(cex.main=0.7)#, family="narrow")
     hm <- heatmap.2(
         mtrans,
         col=colors,
@@ -205,7 +245,7 @@ sampleEnrichmentHeatmap <- function(
         scale="none",
         Rowv=mrowDen,
         Colv=F,
-        margins=c(4, 28),
+        margins=c(4, 22),
         trace="none",
         key.title = "",
         #keysize =0.4,
